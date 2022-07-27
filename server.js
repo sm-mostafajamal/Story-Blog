7b0f42c7547302dade1ad657986ef19702fc9c57
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-07-28 05:09:47 +0600

    adding google auth

--- server.js
+++ server.js
@@ -1,26 +1,48 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const passport = require('passport');
+const session = require('express-session');
 const expressLayouts = require('express-ejs-layouts');
 const connectDB = require('./config/database');
-require('dotenv').config({ path: './config/config.env' });
 const PORT = process.env.PORT || 3000;
 const loginRoutes = require('./routes/index');
+const authRoutes = require('./routes/auth');
+
+
+// Load config
+require('dotenv').config({ path: './config/config.env' });
+// Load passport config
+require('./config/passport')(passport)
 
 connectDB();
 
-// Middlewares
+// express Middlewares, layout
 app.set('view enigne', 'ejs');
-app.use(expressLayouts)
-app.set('layout', './layouts/main.ejs')
-
+app.use(expressLayouts);
+app.set('layout', './layouts/main.ejs');
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));    
+// sesssion middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+
+  }));
+// passport middleware
+app.use(passport.session());
+app.use(passport.initialize());
+// setting the public folder to static
+app.use(express.static('public'));
+// parsing the data in json 
 app.use(express.json());
+// morgan middleware to check the connection logs in console in development mode
 if(process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 
 // Routes
 app.use('/', loginRoutes)
+app.use('/auth', authRoutes)
+
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`));
