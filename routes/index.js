ae0207479cc3e73b748ec19e5a2e20987d532847
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-07-29 02:08:52 +0600

    added authentication

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
 // @desc Login/Landing page
 // @route GET /
-router.get('/', (req, res)=>{
+router.get('/', ensureGuest,  (req, res)=>{
     res.render('login.ejs', { layout: './layouts/login.ejs'})
 })
 
 
 // @desc Dashboard
 // @route GET /dashboard  
-router.get('/dashboard', (req, res)=>{
+router.get('/dashboard', ensureAuth, (req, res)=>{
     res.render('dashboard.ejs')
 })
 module.exports = router;
