080a68ba77ab3546341d4e837e96aeb9dc66ff84
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-07-29 01:55:44 +0600

    added navbar

--- routes/auth.js
+++ routes/auth.js
@@ -12,4 +12,12 @@ router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/'}), (req, res) =>{
     res.redirect('/dashboard')
 })
+
+
+// @desc Logout user
+//  @route /auth/logout
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
 module.exports = router;
