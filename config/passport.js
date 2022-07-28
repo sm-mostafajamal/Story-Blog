cc88dd0fa0c6c5cd4dcc1591ce926bc5a1443e06
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-07-29 01:25:59 +0600

    google auth added

--- config/passport.js
+++ config/passport.js
@@ -9,8 +9,31 @@ module.exports = function(passport) {
         callbackURL: '/auth/google/callback'
     },
     async (accessToken, refreshToken, profile, done) => {
-        console.log(profile)
-    }))
+        const newUser = {
+            googleId: profile.id,
+            displayName: profile.displayName,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            image: profile.photos[0].value
+        }
+
+        try {
+            let user = await User.findOne({ googleId: profile.id});
+            
+            if(user){
+                done(null, user)
+            }else{
+                user = await User.create(newUser)
+                done(null,user)
+            }
+    
+        } catch (e) {
+            console.error(e)
+        }
+
+    }));
+
+    
 
     passport.serializeUser((user, done) =>{
         done(null, user.id)
